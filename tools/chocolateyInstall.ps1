5bcb0c2b7baa67dca60ca15db065434ce6b40f24
Patrick Hufford <pahuff@microsoft.com>
2017-03-22 08:53:35 -0700

    Initial commit
    2.11.115 package tested and uploaded

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'cue';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
+$installerType = 'MSI';
+$url = 'http://downloads.corsair.com/download?item=Files/CUE/CorsairUtilityEngineSetup_2.11.115_release.msi';
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  url64bit      = $url
+  silentArgs    = '/QN'
+  validExitCodes= @(0)
+  softwareName  = 'cue*'
+  checksum      = '5E0325D62BDACF301168C0FAF84A04B9'
+  checksumType  = 'sha256'
+};
+
+Install-ChocolateyPackage @packageArgs;
